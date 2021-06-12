6768bb5dbfd5df06b8c37a47e44df82b60d8112c
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-12 12:24:55 -0700

    updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@ const FILES_TO_CACHE = [
     "./icons/icon-384x384.png",
     // "./icons/icon-512x512.png",
     "./js/index.js",
-    "../../models/transaction.js",
+    // "./models/transaction.js",
 
 ];
 //Intercept fetch requests
