7d71f0e7e54864d85632b9456aaa2f249f474ffb
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-06-12 18:39:56 -0700

    indexed DB working

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,23 +1,18 @@
-// create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'budget-tracker' and set it to version 1
-const request = indexedDB.open('budget-tracker', 1);
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+const request = indexedDB.open('Budget_tracker', 1);
+
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `new_tracker`, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('new_tracker', { autoIncrement: true });
 };
-// upon a successful 
+
 request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
     db = event.target.result;
 
-    // check if app is online, if yes run uploadTracker() function to send all local db data to api
+    // check if app is online, if yes run checkDatabase() function to send all local db data to api
     if (navigator.onLine) {
-        // we haven't created this yet, but we will soon, so let's comment it out for now
-        // uploadPizza();
+        uploadData();
     }
 };
 
@@ -26,14 +21,54 @@ request.onerror = function (event) {
     console.log(event.target.errorCode);
 };
 
-// This function will be executed if we attempt to submit a new tracker and there's no internet connection
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_tracker_record'], 'readwrite');
+    const transaction = db.transaction(['new_tracker'], 'readwrite');
 
-    // access the object store for `new_pizza`
-    const budgetTrackerObjectStore = transaction.objectStore('new_tracker_record');
+    const budgetObjectStore = transaction.objectStore('new_tracker');
 
-    // add record to your store with add method
-    budgetTrackerObjectStore.add(record);
+    // add record to your store with add method.
+    budgetObjectStore.add(record);
 }
+
+function uploadData() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(['new_tracker'], 'readwrite');
+
+    // access your pending object store
+    const budgetObjectStore = transaction.objectStore('new_tracker');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+
+                    const transaction = db.transaction(['new_tracker'], 'readwrite');
+                    const budgetObjectStore = transaction.objectStore('new_tracker');
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+                })
+                .catch(err => {
+                    // set reference to redirect back here
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadData);
